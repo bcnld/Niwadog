1f89410e78a377ffad6987cd6ae3e8f8d8c76053
bcnld <toki20110221@gmail.com>
2025-08-11 02:24:04 +0900

    Update ach.js

--- ach.js
+++ ach.js
@@ -1,1 +1,7 @@
-
+const achievementPanel = document.getElementById('achievement-panel');
+const img = document.createElement('img');
+img.src = 'images/ach_icon.png';  // 画像のパスを必ず正しく
+img.alt = 'Achievement Icon';
+img.style.width = '50px';
+img.style.height = '50px';
+achievementPanel.appendChild(img);
